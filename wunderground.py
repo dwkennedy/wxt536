07d6f90eba0d9c95e8ef5e816fd167bddc4d3245
git@kennedygang.com <github@kennedyang.com>
2020-06-08 17:32:33 -0500

    convert to python 3

--- wunderground.py
+++ wunderground.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 # this server subscribes to mqtt server and retains the last value of each message to serve
@@ -7,7 +7,8 @@
 import time
 import json
 import re
-import urllib
+import urllib.request
+import urllib.error
 import paho.mqtt.client as mqtt
 import math
 import wxFormula
@@ -16,6 +17,11 @@ BROKER_ADDRESS = '127.0.0.1'  # mqtt broker
 WXT_SERIAL = 'N3720229' # PTU S/N N3620062
 BASE_URL = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?"
 PUBLISHING_INTERVAL = 60   # publish every X seconds
+
+WUNDERGROUND_ID = 'KOKNORMA6'  # station ID
+WUNDERGROUND_PASSWORD = '78ce9f30'
+
+current = {}  # create empty dictionary of current observations
 
 # documentation for wunderground PWS update
 # https://support.weather.com/s/article/PWS-Upload-Protocol?language=en_US
@@ -32,28 +38,22 @@ def mm2in(mm):
 def mbar2inhg(mbar):
     return(float(mbar)*0.02952998)
 
-action = 'updateraw'
-ID = 'KOKNORMA6'  # station ID
-height = 375 # station height in meters
-PASSWORD = '78ce9f30'
-dateutc = 'now'
-# set to missing values
-winddir_avg2m = 0 # 'Dm'
-windspeedmph_avg2m = ms2mph(0) # 'Sm'  # convert from m/s to mph
-windgustmph = ms2mph(0)  # 'Sx'  # convert from m/s to mph
-windgustdir = 0  # 'Dx'
-humidity = 0  # 'Ua'
-tempf = C2F(0)  #  'Ta'
-baromin = mbar2inhg(0) # 'Pa'  # convert mbar to inHg
-softwaretype = 'custom'
-dailyrainin = mm2in(0)  # 'Rc', reset at midnight local time
 
-# we don't see rain values often so initialize these
-current = {'Rd':{'value':'0.0'},'Rc':{'value':'0.0'},'Ri':{'value':'0.0'}}
 
 # this code creates the GET request to wunderground
 def createGET(wxt):
-    #print(json.dumps(wxt, indent=4))
+    # set to missing values
+    action = 'updateraw'
+    dateutc = 'now'
+    winddir_avg2m = 0 # 'Dm'
+    windspeedmph_avg2m = ms2mph(0) # 'Sm'  # convert from m/s to mph
+    windgustmph = ms2mph(0)  # 'Sx'  # convert from m/s to mph
+    windgustdir = 0  # 'Dx'
+    humidity = 0  # 'Ua'
+    tempf = C2F(0)  #  'Ta'
+    baromin = mbar2inhg(0) # 'Pa'  # convert mbar to inHg
+    softwaretype = 'custom'
+    dailyrainin = mm2in(0)  # 'Rc', reset at midnight local time
     try:
         winddir_avg2m = wxt['Dm']['value']
         windspeedmph_avg2m = ms2mph(float(wxt['Sm']['value']))
@@ -61,20 +61,18 @@ def createGET(wxt):
         windgustmph = ms2mph(float(wxt['Sx']['value']))
         tempc = float(wxt['Ta']['value'])
         humidity = float(wxt['Ua']['value'])
-        #dewptf = C2F(dewpoint(tempc,humidity))
         dewptf = C2F(float(wxt['Td']['value']))
         tempf = C2F(float(wxt['Ta']['value']))
-        #baromin = mbar2inhg(wxFormula.MSLP(float(wxt['Pa']['value']),height)) 
         baromin = mbar2inhg(float(wxt['Pb']['value'])) 
         dailyrainin = mm2in(float(wxt['Rc']['value']))
     except KeyError:
         print("Missing parameter. check WXT-536 configuration")
-        raise
+        raise  # re-raise error so the caller knows we've failed
 
     url = ''
     url += "action={}".format(action)
-    url += "&ID={}".format(ID)
-    url += "&PASSWORD={}".format(PASSWORD)
+    url += "&ID={}".format(WUNDERGROUND_ID)
+    url += "&PASSWORD={}".format(WUNDERGROUND_PASSWORD)
     url += "&dateutc={}".format(dateutc)
     url += "&winddir={}".format(winddir_avg2m)
     url += "&windspeedmph={:.2f}".format(windspeedmph_avg2m)
@@ -97,55 +95,64 @@ class mqttHandler:
 
     # now we define the callbacks to handle messages we subcribed to
     def on_message(self, client, userdata, message):
-        #print("message received: {0}".format(str(message.payload.decode("utf-8"))))
+        global current
+        #print("message received: {0}".format(message.payload))
         #print("message topic: {0}".format(message.topic))
         #print("message qos: {0}".format(message.qos))
         #print("message retain flag: {0}".format(message.retain))
         # remove leading 'wxt/'
+        components = message.topic.split('/')
         try:
-           components = message.topic.split('/'); 
-           message.topic = components[2];
+           myKey = components[-1]
         except:
            print("Malformed message topic " + message.topic);
-     
-        #print('MQTT: {0}: {1}'.format(message.topic, message.payload))
-        current[str(message.topic.decode('utf-8'))] = json.loads(message.payload);
+
+        myValue = message.payload 
+        #print('MQTT: {}: {}'.format(myKey,myValue))
+        current[myKey] = json.loads(myValue);
 
     def __init__(self):
-
-      # pub/sub to relavent MQTT topics so we can respond to requests with JSON
-      print("{}: {}".format(time.asctime(), "init mqttHandler"))
-      client = mqtt.Client("wunderground")
-      client.on_message = self.on_message
-      client.connect(BROKER_ADDRESS)
-      client.loop_start()
-      client.subscribe('wxt/{}/#'.format(WXT_SERIAL))
-
-# kick off server when the script is called
-if __name__ == '__main__':
+        # pub/sub to relavent MQTT topics so we can respond to requests with JSON
+        print("{}: {}".format(time.asctime(), "init mqttHandler"))
+        client = mqtt.Client("wunderground")
+        client.on_message = self.on_message
+        client.connect(BROKER_ADDRESS)
+        client.loop_start()
+        client.subscribe('wxt/{}/#'.format(WXT_SERIAL))
 
 
+def main():
+    global current
     # fire up mqttHandler to pub/sub to topics
     # should use class factory to pass robot object to httpHandler
+
     robot = mqttHandler()
 
     # send update every minute
     print("{}: {}".format(time.asctime(), "Wunderground client starts"))
     time.sleep(5.1)  # let first messages get published
     timer = time.time()
+    
     while True:
         try:
             url = createGET(current)
             print("{}: {}".format(time.asctime(), url))
-            f = urllib.urlopen(url)
-            print("{}: {}".format(time.asctime(), f.read().strip()))
-            current={}
-            time.sleep(PUBLISHING_INTERVAL-(time.time()-timer))
-            timer = time.time()
+            f = urllib.request.urlopen(url)
+            print("{}: {}".format(time.asctime(), f.read().strip().decode('utf-8')))
+            current={}  # success publishing, clear current
         except KeyError:
-            print('caught KeyError')
-            time.sleep(PUBLISHING_INTERVAL-(time.time()-timer))
-            timer = time.time()
+            print('caught KeyError, missing parameter')
+        except urllib.error.URLError as e:
+            print("URLError {}".format(e))
         except:
-            pass  # usually a urlopen network problem
+            print("some other bizzare error")
+
+        time.sleep(PUBLISHING_INTERVAL-(time.time()-timer))
+        timer = time.time()
+
     print("{}: {}".format(time.asctime(), "Wunderground client stops"))
+
+
+# kick off server when the script is called
+if __name__ == '__main__':
+    main()
