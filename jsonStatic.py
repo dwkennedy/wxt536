757337e53b7d0f4fdfc8f35bccd98b5e734b8b7d
git@kennedygang.com <github@kennedyang.com>
2020-07-04 12:19:41 -0500

    added on_connect so jsonStatic.py will reconnect when broker restarts

--- jsonStatic.py
+++ jsonStatic.py
@@ -6,17 +6,16 @@
 #
 
 import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import json
 import re
-#from urllib.parse import unquote
 import paho.mqtt.client as mqtt
 import logging
 from secret import *
 
-BROKER_ADDRESS = '127.0.0.1'
+#LOCAL_BROKER_ADDRESS is set in secret.py
 WXT_SERIAL = 'N3720229' # PTU S/N N3620062.  Which instrument's data to serve
-TIMEOUT = 20  # seconds before considering current data stale
+TIMEOUT = 15  # seconds before considering current data stale
 TARGET_FILENAME = '/var/www/html/wx/wx.json'
 current = {'valid': 0, 'time': 0}
 
@@ -40,7 +39,15 @@ def write_JSON(wxt):
 #  get data from current dictionary and return values as JSON
 class mqttHandler:
 
-    # now we define the callbacks to handle messages we subcribed to
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("mqttHandler connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe('wxt/{}'.format(WXT_SERIAL))
+
+    # the callback to handle messages we subcribed to
     def on_message(self, client, userdata, message):
         global current
         #print("message topic: {}".format(message.topic))
@@ -53,35 +60,40 @@ class mqttHandler:
             logging.debug(json.dumps(current))
             write_JSON(current) 
         except:
-            print("Failed decoding json")
+            print("mqttHandler failed decoding json")
 
     def __init__(self):
 
       # pub/sub to relavent MQTT topics so we can respond to requests with JSON
-      logging.info("init mqttHandler")
-      client = mqtt.Client("jsonServer")
+      logging.info("mqttHandler init client jsonStatic-{}".format(os.getpid()))
+      client = mqtt.Client("jsonStatic-{}".format(os.getpid()))
+      client.on_connect = self.on_connect
       client.on_message = self.on_message
       client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD);
-      client.connect(BROKER_ADDRESS)
+      client.connect(LOCAL_BROKER_ADDRESS)
       client.loop_start()
-      client.subscribe('wxt/{}'.format(WXT_SERIAL))
+      #client.subscribe('wxt/{}'.format(WXT_SERIAL))
 
 def main():
     global current
 
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
     logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
-    logging.info("jsonServer.py starts - %s", TARGET_FILENAME)
+    logging.info("jsonStatic.py starts - %s", TARGET_FILENAME)
 
     # fire up mqttHandler to pub/sub to topics
     # should use class factory to pass robot object to httpHandler
     robot = mqttHandler()
 
-    while True:
-        time.sleep(20) 
-        write_JSON(current)  # in case we aren't getting new data from mqtt broker
+    try:
+        while True:
+            time.sleep(TIMEOUT) 
+            write_JSON(current)  # in case we aren't getting new data from mqtt broker;
+                                 # bumps clock so web clients know something is up
+    except KeyboardInterrupt:
+        pass
 
-    logging.info("jsonServer.py stops - %s", TARGET_FILENAME)
+    logging.info("jsonStatic.py stops - %s", TARGET_FILENAME)
 
 
 # kick off server when the script is called
