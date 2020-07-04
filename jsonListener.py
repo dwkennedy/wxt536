f9eef088ef9c43f9532f0eeb45a4388e99d594cd
git@kennedygang.com <github@kennedyang.com>
2020-07-04 12:08:08 -0500

    added on_connect function to reconnect automatically when broker restarts

--- jsonListener.py
+++ jsonListener.py
@@ -5,50 +5,60 @@
 # if json file doesn't exist for that day, create a new one
 
 import time
+import os
 import json
 import paho.mqtt.client as mqtt
-#import metTower
-#from netCDF4 import Dataset
 import logging
 from secret import *
 
-BROKER_ADDRESS = '127.0.0.1'
+#LOCAL_BROKER_ADDRESS defined in secret.py
 WXT_SERIAL = 'N3720229' # PTU S/N N3620062.  Which instrument's data to serve
 JSON_OUTPUT_DIRECTORY = '/home/doug/netcdf'
 
-# current holds our incoming mqtt data.  it starts out invalid and out-of-date
-current = {'time': 0}
 
 class mqttHandler:
+    # keep track of current filename/handle. close old file/open new at start of new day
     filename = ""
     filehandle = ""
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("mqttHandler connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe('wxt/{}'.format(WXT_SERIAL))
+
  
-    # now we define the callbacks to handle messages we subcribed to
+    # the callback to handle messages we subcribed to
     def on_message(self, client, userdata, message):
-        global current
         #print("message topic: {}".format(message.topic))
         #print("message received: {}".format(message.payload.decode("utf-8")))
         #print("message qos: {0}".format(message.qos))
         #print("message retain flag: {0}".format(message.retain))
 
+        # current holds our incoming mqtt data.  it starts out invalid and out-of-date
+        current = {'time': 0}
+
         try:
             current = json.loads(message.payload.decode('utf-8'))
             logging.debug(json.dumps(current))
         except:
-            logging.critical("failed decoding incoming json")
+            logging.critical("mqttHandler failed decoding incoming json")
+            return
             #raise
 
-        #  check to see if netcdf file exists
+        #  check to see if json file exists
         (tm_year,tm_mon,tm_mday,tm_hour,tm_min,tm_sec,tm_wday,tm_yday,tm_isdst)=time.gmtime(current['time'])
         new_filename = "{}/clamps_sfc_{:04d}{:02d}{:02d}.json".format(JSON_OUTPUT_DIRECTORY,tm_year,tm_mon,tm_mday)
         if (self.filename != new_filename):
            if (self.filename != ''):
               self.filehandle.close()
            self.filename = new_filename
-           logging.info("opening/appending file {}".format(self.filename))
+           logging.info("mqttHandler opening/appending file {}".format(self.filename))
            self.filehandle = open(self.filename,'a')
  
-        #  insert all the goodies into the netcdf file
+        #  insert all the goodies into the json file
         #print("writing {} to {}".format(current['time'],self.filename))
         #print(self.filehandle)
         json.dump(current, self.filehandle) 
@@ -56,35 +66,41 @@ class mqttHandler:
         self.filehandle.flush()
 
         #print("closing {}".format(filename))
-        #ncfile.close()
 
     def __init__(self):
-
-      # pub/sub to relavent MQTT topics so we can respond to requests with JSON
-      logging.info("init mqttHandler")
-      client = mqtt.Client("netcdfListener")
-      client.on_message = self.on_message
-      client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
-      client.connect(BROKER_ADDRESS)
-      client.loop_start()
-      client.subscribe('wxt/{}'.format(WXT_SERIAL))
+        # pub/sub to relavent MQTT topics so we can respond to requests with JSON
+        logging.info("mqttHandler init client json-listener-%s", os.getpid())
+        client = mqtt.Client("json-listener-{}".format(os.getpid()))
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
+        logging.info('mqttHandler connecting to %s',LOCAL_BROKER_ADDRESS)
+        client.connect(LOCAL_BROKER_ADDRESS)
+        #client.subscribe('wxt/{}'.format(WXT_SERIAL))
+        client.loop_start();  # blocking loop function to handle callbacks, reconnects, etc
+        logging.debug('mqttHandler loop started')
 
 def main():
-    global current
-
     FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-    logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
-
-    # fire up mqttHandler to pub/sub to topics
-    # should use class factory to pass robot object to httpHandler
-    robot = mqttHandler()
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt='%m/%d/%Y %H:%M:%S')
 
     logging.info("jsonListener.py starts")
-    try:
-        while True:
-           time.sleep(60)  
-    except KeyboardInterrupt:
-        pass
+
+#    paho = mqtt.Client('fridge-consumer-%s',(os.getpid()))
+#    paho.on_message = on_message
+#    paho.on_connect = on_connect
+#    paho.username_pw_set(PAHO_USERNAME,PAHO_PASSWORD)
+#    paho.connect(LOCAL_BROKER_ADDRESS)
+#    paho.subscribe('fridge')  # subscribe to fridge status
+
+    # fire up mqttHandler to pub/sub to topics, reconnect when needed
+    robot = mqttHandler()
+
+    while True:
+           logging.debug("mark")
+           time.sleep(60)
+    #except KeyboardInterrupt:
+    #    pass
 
     logging.info("jsonListener.py stops")
 
